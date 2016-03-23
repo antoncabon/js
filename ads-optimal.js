0e49cc6a882800d4008c6c6ff3df0aba87f7a1e3
Antoncabon <antoncabon@users.noreply.github.com>
2016-03-23 23:27:49 +0700

    Create ads-optimal.js

---
+++ ads-optimal.js
@@ -0,0 +1,6 @@
+(function(w) {
+var d=document,h=d.getElementsByTagName('head')[0],j=d.createElement('script'),k=d.createElement('script');
+j.setAttribute('src','//cdn.adsoptimal.com/advertisement/settings/6386.js');
+k.setAttribute('src','//cdn.adsoptimal.com/advertisement/manual.js');
+h.appendChild(j); h.appendChild(k);
+})(window);
