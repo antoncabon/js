99700f522cbc53dea8e7d7d33f6e078fd86503e2
Antoncabon <antoncabon@users.noreply.github.com>
2017-04-18 01:30:08 +0700

    Create showme2.js

---
+++ showme2.js
@@ -0,0 +1,11 @@
+<script type='text/javascript'>
+var currentURL=location.href;
+var str = currentURL;
+var res = str.replace("http://blog.antoncabon.us/p/convert.html?id=", "");
+$(".Visit_Link").hide();
+function changeLink(){
+    var decodedString = Base64.decode(res);
+    window.open(decodedString,'_self');
+    
+}
+</script>
